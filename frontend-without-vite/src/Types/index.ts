076193a4c2eacd45c7036f9eed0f44fd5178246b
Saairaam Prasad K.V. <skv@presidio.com>
2023-12-01 20:01:16 +0530

    restored the  project  from scratch

--- frontend-without-vite/src/Types/index.ts
+++
@@ -1,5 +0,0 @@
-export const ROLE = {
-  USER: 'USER',
-  ADMIN: 'ADMIN',
-};
-export type ModalType = 'add' | 'update' | 'create';
---
+++ frontend/src/components/Genre.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useContext } from 'react'
+import Contextpage from '../Contextpage';
+import {Helmet} from "react-helmet";
+
+function Genre() {
+    const { fetchGenre, activegenre, setActiveGenre, genres ,movies,setFiltered,setMovies, page, setPage, filteredGenre } = useContext(Contextpage);    
+
+
+    useEffect(() => {
+        fetchGenre();  // Fetching Genres on Initial Render.
+    }, [])
+
+
+    return (
+        <>
+        <Helmet>
+            <title>BlueBird Movies | Genres</title>
+        </Helmet>
+
+        <div className='flex flex-wrap justify-center px-2'>
+            {
+                genres.map((genre) => (
+
+                    <button
+                        onClick={() => setActiveGenre(genre.id)}
+                        className={activegenre === genre.id ? 'active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl' : 'px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl'} key={genre.id}>
+                        {genre.name}
+                    </button>
+
+                ))
+            }
+            </div>
+            </>
+    )
+}
+
+export default Genre
